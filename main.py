d1e44d90ce1c767dbf08079f977adbfcd3588589
youaremagic <145214127+youaremagic@users.noreply.github.com>
2023-12-02 08:55:39 +0530

    added confirmation dialogues, modified requirements.txt

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from utils import app, variables
 def main(page: ft.Page):
     page.title = "Easy Markdown"
     page.theme = ft.theme.Theme(color_scheme_seed=ft.colors.PURPLE_300)
+    page.window_prevent_close = True
+    page.on_window_event=app.window_event
     variables.update_variables(page)
     page.add(app.content())
 
--- utils/app.py
+++ utils/app.py
@@ -4,15 +4,94 @@ from utils import variables as var
 from components import snackbar
 
 FILE_PATH=None
+FILE_CHECKPOINT=''
 
 file_indicator=ft.Text(f"File : {FILE_PATH}")
+
+def is_content_modified():
+    if FILE_CHECKPOINT != markdown_text_field.value:
+        return True
+    else:
+        return False
+
+def exit_app(e):
+    e.page.window_destroy()
+
+def cancel_dialog():
+    exit_confirm_dialog.open = False
+    open_confirm_dialog.open = False
+    close_confirm_dialog.open = False
+    var.PAGE.update()
+
+def save_file_and_exit(e):
+    save_to_file()
+    cancel_dialog()
+
+dialog_title=ft.Text("Please confirm")
+dialog_content=ft.Text("You have unsaved changes!")
+
+exit_confirm_dialog = ft.AlertDialog(
+    modal=True,
+    title=dialog_title,
+    content=dialog_content,
+    actions=[
+        ft.OutlinedButton("Save", on_click=lambda _:save_to_file()),
+        ft.OutlinedButton("Exit without saving", on_click=exit_app),
+        ft.OutlinedButton("Cancel", on_click=lambda _:cancel_dialog()),
+    ],
+    actions_alignment=ft.MainAxisAlignment.SPACE_AROUND,
+)
+
+open_confirm_dialog = ft.AlertDialog(
+    modal=True,
+    title=dialog_title,
+    content=dialog_content,
+    actions=[
+        ft.OutlinedButton("Save", on_click=lambda _:save_to_file()),
+        ft.OutlinedButton("Ignore changes", on_click=lambda _:open_file(force=True)),
+        ft.OutlinedButton("Cancel", on_click=lambda _:cancel_dialog()),
+    ],
+    actions_alignment=ft.MainAxisAlignment.SPACE_AROUND,
+)
+
+close_confirm_dialog = ft.AlertDialog(
+    modal=True,
+    title=dialog_title,
+    content=dialog_content,
+    actions=[
+        ft.OutlinedButton("Save", on_click=save_file_and_exit),
+        ft.OutlinedButton("Ignore Changes", on_click=lambda _:close_file(force=True)),
+        ft.OutlinedButton("Cancel", on_click=lambda _:cancel_dialog()),
+    ],
+    actions_alignment=ft.MainAxisAlignment.SPACE_AROUND,
+)
+
+def show_confirm_dialog(action):
+    if action=='exit':
+        var.PAGE.dialog = exit_confirm_dialog
+        exit_confirm_dialog.open = True
+    elif action=='close':
+        var.PAGE.dialog = close_confirm_dialog
+        close_confirm_dialog.open = True
+    elif action=='open':
+        var.PAGE.dialog = open_confirm_dialog
+        open_confirm_dialog.open = True
+    var.PAGE.update()
+
+def window_event(e):
+    page=e.page
+    if e.data == 'close':
+        if is_content_modified() == True:
+            show_confirm_dialog('exit')
+        else:
+            exit_app(e)
 
 def update_file_indicator():
     file_indicator.value=f"File : {FILE_PATH}"
     file_indicator.update()
 
 def pick_files_result(e: ft.FilePickerResultEvent):
-    global FILE_PATH
+    global FILE_PATH, FILE_CHECKPOINT
     if e.files:
         for file in e.files:
             FILE_PATH=file.path
@@ -24,6 +103,7 @@ def pick_files_result(e: ft.FilePickerResultEvent):
                 markdown_output.update()
                 file.close()
                 update_file_indicator()
+                FILE_CHECKPOINT=file_content
 
 def save_file_result(e: ft.FilePickerResultEvent):
     global FILE_PATH
@@ -33,22 +113,43 @@ def save_file_result(e: ft.FilePickerResultEvent):
         update_file_indicator()
 
 def save_to_file():
+    cancel_dialog()
+    global FILE_CHECKPOINT
     if FILE_PATH:
         with open(FILE_PATH, "w") as file:
             file.write(markdown_text_field.value)
             file.close()
             snackbar.show_snackbar(f"Saved as {FILE_PATH}")
+            FILE_CHECKPOINT=markdown_text_field.value
     else:
         file_saver.save_file(file_type=ft.FilePickerFileType.CUSTOM, allowed_extensions=['md'])
 
-def close_file():
-    global FILE_PATH
-    FILE_PATH=None
-    markdown_text_field.value=''
-    markdown_output.value=''
-    markdown_text_field.update()
-    markdown_output.update()
-    update_file_indicator()
+def close_file(force=False): # force is used to close without showing confirm dialog
+    if is_content_modified() == True and force==False:
+        show_confirm_dialog('close')
+    else:
+        cancel_dialog()
+        global FILE_CHECKPOINT
+        markdown_text_field.value=''
+        markdown_output.value=''
+        markdown_text_field.update()
+        markdown_output.update()
+        update_file_indicator()
+        FILE_CHECKPOINT=''
+
+def open_file(force=False): # force is used to open without showing confirm dialog
+    if is_content_modified() == True and force==False:
+        show_confirm_dialog('open')
+    else:
+        cancel_dialog()
+        file_picker.pick_files(
+            allow_multiple=False,
+            allowed_extensions=['md'],
+            file_type=ft.FilePickerFileType.CUSTOM
+        )
+
+def new_document():
+    close_file()
 
 file_picker=ft.FilePicker(
     on_result=pick_files_result,
@@ -80,6 +181,8 @@ markdown_output=ft.Markdown(
 )
 
 def render_markdown():
+    global FILE_STATE
+    FILE_STATE=1
     markdown_output.value=markdown_text_field.value
     markdown_output.update()
 
@@ -100,13 +203,14 @@ def content():
             ft.PopupMenuButton(
                 items=[
                     ft.PopupMenuItem(
+                        icon=ft.icons.FILE_COPY,
+                        text='New',
+                        on_click=lambda _:new_document(),
+                    ),
+                    ft.PopupMenuItem(
                         icon=ft.icons.FILE_OPEN,
                         text='Open',
-                        on_click=lambda _: file_picker.pick_files(
-                            allow_multiple=False,
-                            allowed_extensions=['md'],
-                            file_type=ft.FilePickerFileType.CUSTOM
-                        )
+                        on_click=lambda _:open_file(),
                     ),
                     ft.PopupMenuItem(
                         icon=ft.icons.SAVE,
